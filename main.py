d4ce233933cca1b6b7f655774e457f6886237d2f
Jason Miller <Jason@example.com>
2025-08-31 15:48:41 -0700

    finished drawing player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-
+from circleshape import *
+from player import *
 def main():
     pygame.init()
     
@@ -15,12 +16,15 @@ def main():
 
     dt = 0
 
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0)
         dt = clock.tick(60) / 1000
+        player_1.draw(screen)
         
         pygame.display.flip()
 
